dd44525ce2b8aad714d30ab27809ea59b89b7427
RAMCHARAN-YARABATI <ramcharanyarabati35@gmail.com>
2025-10-09 14:54:58 +0530

    Delete accounts/create_superuser.py

--- accounts/create_superuser.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth import get_user_model
-
-def run():
-    User = get_user_model()
-    if not User.objects.filter(username="admin").exists():
-        User.objects.create_superuser("ram", "ram@gmail.com", "123")
-        print("Superuser created")
-    else:
-        print("Superuser already exists")
