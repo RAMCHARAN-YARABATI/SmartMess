041f2b5d8a17827cb5bcfd3722957a31d17551a3
RAMCHARAN-YARABATI <ramcharanyarabati35@gmail.com>
2025-10-09 17:14:10 +0530

    Update settings.py

--- Smart_Meal_Management_System/settings.py
+++ Smart_Meal_Management_System/settings.py
@@ -127,7 +127,7 @@ USE_I18N = True
 USE_TZ = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'noreplymymessbooking@gmail.com'
