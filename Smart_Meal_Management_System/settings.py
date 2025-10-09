3f3737d5c190a71e3931fb32a298cd88cec644d8
RAMCHARAN-YARABATI <ramcharanyarabati35@gmail.com>
2025-10-09 21:50:35 +0530

    Update settings.py

--- Smart_Meal_Management_System/settings.py
+++ Smart_Meal_Management_System/settings.py
@@ -126,16 +126,13 @@ USE_I18N = True
 
 USE_TZ = True
 
-import os
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'apikey'  # literally the word 'apikey'
-EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_API_KEY')
-DEFAULT_FROM_EMAIL = 'noreplymymessbooking@gmail.com'  # your verified sender
- 
+EMAIL_HOST_USER = 'noreplymymessbooking@gmail.com'
+EMAIL_HOST_PASSWORD = 'smlz qwcw bksm wvlb'  
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
