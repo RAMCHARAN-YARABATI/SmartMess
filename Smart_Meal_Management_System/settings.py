ce446d61601abdec9f4bde3eee8fa9885140a165
RAMCHARAN-YARABATI <ramcharanyarabati35@gmail.com>
2025-10-10 19:24:46 +0530

    Update settings.py

--- Smart_Meal_Management_System/settings.py
+++ Smart_Meal_Management_System/settings.py
@@ -152,7 +152,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'noreplymymessbooking@gmail.com'
-EMAIL_HOST_PASSWORD = 'smlz qwcw bksm wvlb'  
+EMAIL_HOST_PASSWORD = 'smlz qwcw bksm wvlb' 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
@@ -170,3 +170,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
