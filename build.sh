c462b5d5e870ea9596fe80d5c8b274dfe28a2018
RAMCHARAN-YARABATI <ramcharanyarabati35@gmail.com>
2025-10-11 09:36:09 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,16 @@
- echo "BUILD START"
- python3.9 -m pip install -r requirements.txt
- python3.9 manage.py collectstatic --noinput --clear
- echo "BUILD END"
+Bash
+
+#!/bin/bash
+
+# 1. Install Python dependencies
+echo "Installing dependencies from requirements.txt..."
+pip install -r requirements.txt
+
+# 2. Run migrations
+echo "Running database migrations..."
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+
+# 3. Collect static files
+echo "Collecting static files..."
+python manage.py collectstatic --noinput --clear
