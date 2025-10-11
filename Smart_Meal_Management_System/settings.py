70bc96dd5017019ef7b05e36c99b30e2a96a4e32
RAMCHARAN-YARABATI <ramcharanyarabati35@gmail.com>
2025-10-11 10:15:20 +0530

    Update settings.py

--- Smart_Meal_Management_System/settings.py
+++ Smart_Meal_Management_System/settings.py
@@ -160,7 +160,7 @@ EMAIL_HOST_PASSWORD = 'smlz qwcw bksm wvlb'
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
@@ -170,5 +170,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
 
 
