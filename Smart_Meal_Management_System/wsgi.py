5bd016d4ab628cf51763ad60b4b82312d09138b7
RAMCHARAN-YARABATI <ramcharanyarabati35@gmail.com>
2025-10-11 11:47:11 +0530

    Update wsgi.py

--- Smart_Meal_Management_System/wsgi.py
+++ Smart_Meal_Management_System/wsgi.py
@@ -9,9 +9,15 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from Smart_Meal_Management_System.settings import BASE_DIR
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Smart_Meal_Management_System.settings')
 
 application = get_wsgi_application()
+
+from whitenoise import WhiteNoise
+
+application = get_wsgi_application()
+application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles_build'))
 
 app = application
