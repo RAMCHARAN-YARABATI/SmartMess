330c14a08dc5a11044331b7814b252df85f4e6e8
RAMCHARAN-YARABATI <ramcharanyarabati35@gmail.com>
2025-10-11 09:55:54 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,14 @@
-Bash
-
-#!/bin/bash
+#!/usr/bin/env bash
 
 # 1. Install Python dependencies
 echo "Installing dependencies from requirements.txt..."
-pip install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
 
 # 2. Run migrations
 echo "Running database migrations..."
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
 
 # 3. Collect static files
 echo "Collecting static files..."
-python manage.py collectstatic --noinput --clear
+python3.9 manage.py collectstatic --noinput --clear
