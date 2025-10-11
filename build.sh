b963cffd54a52adc5db8aee037021864094c1110
RAMCHARAN-YARABATI <ramcharanyarabati35@gmail.com>
2025-10-11 11:35:58 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
 
-# 1. Install dependencies using the globally known pip3 executable
-echo "Installing dependencies from requirements.txt..."
-pip3 install -r requirements.txt
-
-# 2. Run migrations using the globally known python3 executable
+# 1. Run migrations
 echo "Running database migrations..."
 python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
 
-# 3. Collect static files
+# 2. Collect static files
 echo "Collecting static files..."
 python3 manage.py collectstatic --noinput --clear
