115b300484201728b4ce246c4c8ae0385dddf5ca
RAMCHARAN-YARABATI <ramcharanyarabati35@gmail.com>
2025-10-11 10:07:46 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 
-# 1. Install Python dependencies using explicit paths and -m pip
-# This format is the most robust way to ensure installation runs
+# 1. Install Python dependencies using the global pip3 executable
 echo "Installing dependencies from requirements.txt..."
-/usr/bin/python3.9 -m pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 # 2. Run migrations
 echo "Running database migrations..."
-/usr/bin/python3.9 manage.py makemigrations --noinput
-/usr/bin/python3.9 manage.py migrate --noinput
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
 
 # 3. Collect static files
 echo "Collecting static files..."
-/usr/bin/python3.9 manage.py collectstatic --noinput --clear
+python3 manage.py collectstatic --noinput --clear
